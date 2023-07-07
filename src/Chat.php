19cb98143b04e8df1e23f37f3248de4cae9f3cc4
kelly-olsson <31006090+kelly-olsson@users.noreply.github.com>
2023-07-06 21:32:39 -0700

    Add comments to relevant code for presentation

--- src/Chat.php
+++ src/Chat.php
@@ -1,64 +1,108 @@
 <?php
 namespace MyApp;
-use Ratchet\MessageComponentInterface;
-use Ratchet\ConnectionInterface;
+// This defines the namespace for the app.
 
-/**
- * NOTE: resourceId is not a part of ConnectionInterface, but is accessible because of the actual type of object
- * received (IoConnection) in $conn->resourceId.
- */
+use Ratchet\MessageComponentInterface;
+// This imports the MessageComponentInterface from the Ratchet library. This interface defines the necessary methods
+// for a WebSocket application.
+
+use Ratchet\ConnectionInterface;
+// This imports the ConnectionInterface from the Ratchet library. This interface represents a connection to the
+// server from a client.
+
 class Chat implements MessageComponentInterface {
+// The Chat custom class implements MessageComponentInterface. This is an interface that your application class
+// implements to react to events that occur on the WebSocket, such as when a connection is opened, a message is
+// received, or an error occurs.
+
     protected \SplObjectStorage $clients;
+    // SplObjectStorage is a built-in PHP class that allows objects to be stored and managed.
+    // Here it's used to store all client connections.
 
     public function __construct() {
         $this->clients = new \SplObjectStorage;
     }
+    // In the constructor of the class, $this->clients is initialized as a new SplObjectStorage instance.
 
     public function onOpen(ConnectionInterface $conn): void
     {
         $this->clients->attach($conn);
         echo "New connection! ({$conn->resourceId})\n";
     }
+    // The onOpen method is called when a new connection is established. The client's connection is added to the
+    // $clients SplObjectStorage.
+    // NOTE: resourceId is not a part of ConnectionInterface, but is accessible because of the actual type of object
+    // received (IoConnection, a subclass of ConnectionInterface) in $conn->resourceId.
 
     public function onMessage(ConnectionInterface $from, $msg): void
     {
+        // This method is invoked whenever a new message is received from a connection.
+
         $msg = json_decode($msg);
+        // The incoming message is in JSON format, so we use json_decode to convert it into a PHP object.
+
         $type = $msg->type;
+        // We extract the type of the message. The type will help us decide how to handle this message.
 
         switch ($type) {
+            // We switch on the message type to handle different types of messages.
+
             case 'open':
+                // This case is when a new connection is opened.
+
                 $user_id = $from->resourceId;
+                // We assign the resource ID of the connection to user_id, which will be unique for each connection.
+
                 $chat_msg = "";
+                // As this is an 'open' type message, no chat message is associated with it.
 
                 $from->send(json_encode(array("type"=>$type,"msg"=>$chat_msg, "user_id"=>$user_id, "is_it_me"=>true)));
+                // We then send a JSON-encoded message back to the client who just connected, marking "is_it_me" as true to distinguish that this message is for the user who just connected.
+
                 foreach($this->clients as $client){
+                    // We iterate over all other clients...
                     if($from !== $client)
+                        // ...and if the client is not the one who just connected...
                         $client->send(json_encode(array("type"=>$type,"msg"=>$chat_msg, "user_id"=>$user_id, "is_it_me"=>false)));
+                    // ...we send them a JSON-encoded message too, but with "is_it_me" marked as false.
                 }
                 break;
+
             case 'chat':
+                // This case is when an existing connection sends a chat message.
+
                 $user_id = $from->resourceId;
+                // As before, we use the resource ID as the user_id.
+
                 $chat_msg = $msg->chat_msg;
+                // This time, we have a message from the user, which we extract from the 'chat_msg' field of the incoming message.
 
                 $from->send(json_encode(array("type"=>$type,"msg"=>$chat_msg, "user_id"=>$user_id, "is_it_me"=>true)));
+                // As before, we send a JSON-encoded message back to the user who sent the chat message, marking "is_it_me" as true.
+
                 foreach($this->clients as $client){
+                    // We iterate over all other clients...
                     if($from !== $client)
+                        // ...and if the client is not the one who sent the chat message...
                         $client->send(json_encode(array("type"=>$type,"msg"=>$chat_msg, "user_id"=>$user_id, "is_it_me"=>false)));
+                    // ...we send them a JSON-encoded message too, but with "is_it_me" marked as false.
                 }
                 break;
         }
     }
+    // The onMessage method is called whenever a message is received from a client. It handles different types of messages based on the value of the 'type' field in the message.
 
     public function onClose(ConnectionInterface $conn): void
     {
         $this->clients->detach($conn);
-        // unset($this->users[$conn->resourceId]);
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
+    // The onClose method is called when a connection is closed. The client's connection is removed from the $clients SplObjectStorage.
 
     public function onError(ConnectionInterface $conn, \Exception $e): void
     {
         echo "An error has occurred: {$e->getMessage()}\n";
         $conn->close();
     }
+    // The onError method is called when an error occurs. It outputs the error message and closes the connection.
 }
--- src/index.php
+++ src/index.php
@@ -30,30 +30,39 @@
         // Scroll to bottom of chat on new message
         const scrollToBottom = () => $chat_output.scrollTop($chat_output[0].scrollHeight);
 
+        // WebSocket event handler for when the connection opens
         websocket_server.onopen = function () {
+            // When the connection is open, send a message to the server indicating the connection is open
             websocket_server.send(JSON.stringify({
                 'type': 'open'
             }));
         };
 
+        // WebSocket event handler for receiving messages from the server
         websocket_server.onmessage = function (e) {
+            // Try to parse the received message as JSON
             try {
                 const json = JSON.parse(e.data);
                 let $alert, $chat;
 
+                // Process the received message based on its type
                 switch (json.type) {
                     case 'open':
+                        // When a new user opens a connection, add a message to the chat
                         if (!json.is_it_me) {
+                            // If it's not the current user who joined, print a message that a new user has joined
                             $alert = $(`<div>--- New user <strong>${json.user_id}</strong> just joined this chat room. ---</div><br>`).addClass('text-center');
                             $chat_output.append($alert);
                             scrollToBottom();
                         } else {
+                            // If it's the current user who just connected, print a welcoming message
                             $alert = $(`<div>Welcome to the chat room! Your user id is <strong>${json.user_id}</strong>.</div>`).addClass('alert alert-primary');
                             $chat_output.append($alert);
                             scrollToBottom();
                         }
                         break;
                     case 'chat':
+                        // When a user sends a chat message, add it to the chat
                         $chat = $(`<div class="chat-message ${json.is_it_me ? 'my-message' : 'other-message'}"><strong>${json.is_it_me ? 'You say:' : 'User ' + json.user_id + ' says:'}</strong><br/>${json.msg}</div>`);
                         let $chat_container = $(`<div class="chat-container ${json.is_it_me ? 'my-message-container' : ''}"></div>`).append($chat);
                         $chat_output.append($chat_container);
@@ -65,6 +74,7 @@
             }
         };
 
+        // WebSocket event handler for errors
         websocket_server.onerror = function (e) {
             console.error('WebSocket Error:', e);
             const $error_msg = $("<div>There was a problem while sending your message.</div>").addClass('alert alert-danger');
@@ -72,16 +82,19 @@
             scrollToBottom();
         }
 
-        // Events
+        // Event handler for keyup event on the chat input field
         $('#chat_input').on('keyup', function (e) {
+            // If the key pressed was 'Enter' and not 'Shift+Enter', send the chat message
             if (e.keyCode === 13 && !e.shiftKey) {
                 const chat_msg = $(this).val();
 
+                // Send the chat message to the server
                 websocket_server.send(JSON.stringify({
                     'type': 'chat',
                     'chat_msg': chat_msg
                 }));
 
+                // Clear the input field after sending the message
                 $(this).val('');
             }
         });
